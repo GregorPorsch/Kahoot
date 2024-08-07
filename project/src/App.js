0a0eb5fe86ee85eb1913d243b807cf59f41d890e
Gregor Porsch <porschgregor@gmail.com>
2024-08-07 15:21:49 +0200

    Verbesserungen von Join

--- project/src/App.js
+++ project/src/App.js
@@ -1,83 +1,37 @@
-// App.js
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import io from "socket.io-client";
-import axios from "axios";
+
+import Home from "./components/Home";
+import Game from "./components/Game";
 
 const socket = io("http://localhost:5000");
 
 function App() {
-  const [gameId, setGameId] = useState("");
   const [username, setUsername] = useState("");
-  const [joined, setJoined] = useState(false);
-
-  useEffect(() => {
-    socket.on("user_joined", (data) => {
-      console.log(`${data.username} ist dem Spiel beigetreten.`);
-    });
-
-    socket.on("user_left", (data) => {
-      console.log(`${data.username} hat das Spiel verlassen.`);
-    });
-
-    return () => {
-      socket.off("user_joined");
-      socket.off("user_left");
-    };
-  });
-
-  const createGame = async () => {
-    const response = await axios.post("http://localhost:5000/create_game");
-    setGameId(response.data.game_id);
-  };
-
-  const joinGame = async () => {
-    const response = await axios.post("http://localhost:5000/join_game", {
-      game_id: gameId,
-      username,
-    });
-    if (response.data.status === "joined") {
-      socket.emit("join", { game_id: gameId, username });
-      setJoined(true);
-    }
-  };
-
-  const leaveGame = async () => {
-    const response = await axios.post("http://localhost:5000/leave_game", {
-      game_id: gameId,
-      username,
-    });
-    if (response.data.status === "left") {
-      socket.emit("leave", { game_id: gameId, username });
-      setJoined(false);
-    }
-  };
+  const [joined, setJoined] = useState(true);
 
   return (
-    <div>
-      <h1>Kahoot</h1>
-      <button onClick={createGame}>Neues Spiel erstellen</button>
-      <div>
-        <input
-          type="text"
-          placeholder="Spiel-ID"
-          value={gameId}
-          onChange={(e) => setGameId(e.target.value)}
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              username={username}
+              setUsername={setUsername}
+              joined={joined}
+              setJoined={setJoined}
+              socket={socket}
+            />
+          }
         />
-        <input
-          type="text"
-          placeholder="Benutzername"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+        <Route
+          path="/game/:gameId"
+          element={<Game username={username} setJoined={setJoined} socket={socket} />}
         />
-        <button onClick={joinGame}>Spiel beitreten</button>
-        <button onClick={leaveGame}>Spiel verlassen</button>
-      </div>
-      {joined && (
-        <p>
-          {username} ist dem Spiel {gameId} beigetreten.
-        </p>
-      )}
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
